dfbaf849629016101d0a22fd81fb0560fa576f17
Eliel Herrera Gress <elieta103@gmail.com>
2024-01-22 20:37:18 -0600

    05/cors-csrf

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
-## Spring Security 04
+## Spring Security 05
 - Crear una nueva rama 
-  - git checkout -b "04/authentication_provider"
+  - git checkout -b "05/cors-csrf"
 
-- Implementacion Authentication Provider
-  - Es una alternativa al uso de UserDetails
-  - Eliminar MyPasswordEncoder, CustomerUserDetails
-  - En SecurityConfig Regresar a :  NoOpPasswordEncoder
-  - Agregar :
-    - @Component
-    - public class MyAuthenticationProvider implements AuthenticationProvider{...}
-  - Es opcional deshabilitar CORS y CSRF
-    - http.cors(AbstractHttpConfigurer::disable);
-    - http.csrf(AbstractHttpConfigurer::disable);
+- Levantando el front y back manda el error :
+  - Desde postman funciona.
+  - Desde el browser no funciona.
+    - Access to XMLHttpRequest at 'http://localhost:8080/welcome' from origin 'http://localhost:4200' 
+    - has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+    
+  - Implementar las politicas de CORS en :
+    - @Bean
+    - CorsConfigurationSource corsConfigurationSource(){...}
+    - Agregar en SecurityConfig :
+      - http.cors(cors-> corsConfigurationSource());
+  
+  - Implementar las politicas de CSRF en :
+    - Crear filtro para validar cookie de CSRF, Genera el token y lo setea en los headers.
+    - public class CsrfCookieFilter extends OncePerRequestFilter {...} 
+    - Agregar en SecurityConfig :
+      - var requestHandler = new CsrfTokenRequestAttributeHandler();
+      - requestHandler.setCsrfRequestAttributeName("_csrf");
+      - 
+      - // En todas las url se envía el token, pero se ignora en /welcome y /about_us
+      - http.csrf(csrf-> csrf.csrfTokenRequestHandler(requestHandler)
+      -  .ignoringRequestMatchers("/welcome","/about_us")
+      -  .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+      -  .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+  - En los Headers del response se agregan :
+    - XSRF-TOKEN=0eaf31b6-c409-41e3-b144-113f2c9373b1; 
+    - X-Xsrf-Token:0eaf31b6-c409-41e3-b144-113f2c9373b1
 
   - Para agregar codigo en rama :
-    - git checkout -b "04/authentication_provider"  (SE REALIZÓ AL INICIO)
+    - git checkout -b "05/cors-csrf"  (SE REALIZÓ AL INICIO)
     - git status
     - git add .
-    - git commit -m "04/authentication_provider"
-    - git push -u origin 04/authentication_provider
+    - git commit -m "05/cors-csrf"
+    - git push -u origin 05/cors-csrf
--- src/main/java/com/elhg/security/security/SecurityConfig.java
+++ src/main/java/com/elhg/security/security/SecurityConfig.java
@@ -11,23 +11,55 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        var requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/loans","/balance", "/accounts", "/cards").authenticated()
                         .anyRequest().permitAll())
                         //.requestMatchers("/welcome", "/about_us").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-        //http.cors(AbstractHttpConfigurer::disable);
-        //http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(cors-> corsConfigurationSource());
+        // En todas las url se envía el token, pero se ignora en /welcome y /about_us
+        http.csrf(csrf-> csrf.csrfTokenRequestHandler(requestHandler)
+                .ignoringRequestMatchers("/welcome","/about_us")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        var config = new CorsConfiguration();
+        //config.setAllowedOrigins(List.of("http://localhost:4200", "http://my-app.com"));
+        //config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return  source;
     }
 
     /*@Bean
